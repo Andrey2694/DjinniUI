94a10f12fa465a2f16c95fcd1c3d7352d469dfdc
Andrey2694 <fordcoppola94@gmail.com>
2022-01-06 19:51:43 +0200

    added test for assert links

--- src/test/java/tests/DjinniJobsTest.java
+++ src/test/java/tests/DjinniJobsTest.java
@@ -1,10 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
 import helpers.DriverUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -12,6 +17,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Tag("djinni")
 public class DjinniJobsTest extends BaseTest {
+
+    @Test
+    @DisplayName("Open links from vacancies and check that they are not 404")
+    void openLinksFromVacancies() {
+        step("Open website", () -> {
+            open("jobs/");
+        });
+
+        step("Open links from each vacancy on the page", () -> {
+            List<String> links = $$x("//li[@class = 'list-jobs__item']//a[@href]").stream().map(x -> x.getAttribute("href")).collect(Collectors.toList());
+            for (String url : links) {
+                open(url);
+                String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+
+                step("Check that page is not 404", () -> {
+                    assertThat(currentUrl).isEqualTo(url);
+                    $x("//h2[text() = '404']").shouldNotBe(Condition.exist);
+                });
+            }
+        });
+    }
 
     @Test
     @DisplayName("Vacancy items are displayed on the page")
